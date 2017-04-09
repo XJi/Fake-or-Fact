97005783f2d0ea33b0762ce23d0b15d4fbbc3bc6
brianedmonds90 <brianedmonds90@gmail.com>
2017-04-09 14:26:37 -0400

    added average creation date calc

--- domain_analysis.py
+++ domain_analysis.py
@@ -1,7 +1,8 @@
 import csv
+import cPickle as pickle
 from urlparse import urlparse
 import sys
-
+import whois
 
 def isInDictionary(d,url):
 	list_sites = d.keys()
@@ -45,6 +46,51 @@ def isDomainReputable(url):
 		return value
 	return "Site Not Found in our data list!"
 
+#returns a dictioanry of relevant whois information to help 
+def getWhoisCreationDate(hostname):
+	whois_d = {}
+	w = whois.whois(hostname)
+	creation_date = w['creation_date']
+	#print creation_date
+	#print hostname
+	if hostname == 'independent.co.uk':
+		yr = 1996
+		return yr
+	yr = 0	
+	try:
+		for obj in creation_date:
+			if yr< obj.year:
+				yr = obj.year
+	except TypeError:
+		yr = creation_date.year	
+	return yr
+
+def calcAvgCreationDateAge(list_domains):
+	list_dates = []
+	sum = 0
+	for dom in list_domains:
+		yr = getWhoisCreationDate(dom)
+		print yr, dom
+		sum+=yr
+		list_dates.append(yr)
+	return float(sum)/float(len(list_dates))	
+#	pickle.dump(list_dates,'creation_dates.cp')
+#	print list_dates
+
+def getListOfNewsDomains(file_path):
+	list_dom =[]
+	with open(file_path,'r') as csvfile:
+		my_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+		#need to skip the first line of the site
+		my_reader.next()
+		for row in my_reader:
+			site = row[0]
+			if site.startswith('"') and site.endswith('"'):
+			    site = site[1:-1]
+			list_dom.append(site)
+	return list_dom	
+
+
 
 if __name__ == "__main__":
 	if len(sys.argv)<2:
@@ -55,5 +101,9 @@ if __name__ == "__main__":
 		print urlparse(url2).hostname + " is considered "+isDomainReputable(url2)
 	else:
 		url= sys.argv[1]
-		print urlparse(url).hostname + " is considered "+isDomainReputable(url)
-		
+		news_hostname = urlparse(url).hostname 
+		print news_hostname+ " is considered "+isDomainReputable(url)
+		#print "News Domain was created on "+str( getWhoisInformation(news_hostname))
+		list_credible_domains = getListOfNewsDomains('credible.csv')
+		print('Avg Domain Creation Date of Credible News Sites is 1994.95 ')
+		print(news_hostname+' was first registered on '+str(getWhoisCreationDate(news_hostname)))
